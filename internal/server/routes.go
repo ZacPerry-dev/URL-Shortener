699c9a1a021c417b176400b83b32b23894052287
zperry4 <zactperry73@gmail.com>
2024-01-10 19:29:13 -0500

    add home route

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,6 +7,7 @@ import (
 func (s *Server) RegisterRoutes() http.Handler {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/home", s.Home)
 	mux.HandleFunc("/", s.GetURL)
 	mux.HandleFunc("/addURL", s.PostURL)
 	mux.HandleFunc("/deleteURL", s.DeleteURL)
