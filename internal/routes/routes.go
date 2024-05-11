ffc191ffb23c5b1ac6ee592f2c47b816ae95283f
zperry4 <zactperry73@gmail.com>
2024-05-11 19:43:21 -0400

    refactor handlers, routes. Remove old files

---
+++ internal/routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"URL-Shortener/internal/database"
+	"URL-Shortener/internal/handlers"
+	"net/http"
+)
+
+func AddRoutes(mux *http.ServeMux, db *database.Database) {
+	h := handlers.NewHandler(db)
+
+	// Handle requests
+	mux.Handle("/home", h.HandleHomePageRequest())
+	mux.Handle("/", h.HandleGetURL())
+	mux.Handle("/shorten", h.HandleShortenURL())
+	mux.Handle("/delete", h.HandleDeleteURL())
+
+	// Serve static files (This is just here for now. Idk where else to put this yet...)
+	fileServer := http.FileServer(http.Dir("./views/static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+}
